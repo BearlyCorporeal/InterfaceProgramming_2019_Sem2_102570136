daa0dbea6e86151bf60023606da1db1294b540f1
BearlyCorporeal <48078310+BearlyCorporeal@users.noreply.github.com>
2019-08-13 17:01:23 +1000

    continued work on the app
    see above

--- week 3/my-app/src/app/arrays/arrays.component.ts
+++ week 3/my-app/src/app/arrays/arrays.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { filter } from 'minimatch';
 
+class todo {
+  title: string;
+  datestarted: Date;
+  compleated:boolean;
+
+
+  constructor(title:string, datestarted: Date, compleated:boolean){
+    this.title = title;
+    this.datestarted = datestarted;
+    this.compleated = compleated; 
+  }
+}
+
 class hero {
   name: string;
   age: number;
@@ -22,6 +35,7 @@ export class ArraysComponent implements OnInit {
 
   names = ["person1","person2","person3"];
   heros = [new hero("hero1",785),new hero("hero2",784)]
+  todos = [new todo("ad some todos",time.year.date.now,false)]
   constructor() { }
 
   ngOnInit() {
@@ -41,8 +55,15 @@ export class ArraysComponent implements OnInit {
     var index = this.heros.indexOf(hero);
     this.heros.splice(index, 1);
   }
+  removetodos(hero){
+    var index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
   addhero(heroname: string,heroage :number){
     this.heros.push(new hero(heroname,heroage));
+  }
+  addtodo(todotitle: string,){
+    this.todos.push(new todos(todotitle,time.now,false));
   }
 
 }
